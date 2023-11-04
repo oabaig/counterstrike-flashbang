37fee88b8a83e45764711645c31ae78c0fa4ac6b
Omar Baig <OmarBaig1998@gmail.com>
2023-11-04 00:24:54 -0500

    update main

--- src/host.py
+++ src/host.py
@@ -3,31 +3,29 @@ from PIL import ImageGrab
 import cv2
 import os
 from dotenv import load_dotenv
-
 import socket
 
-host = os.getenv('HOST')
-port = int(os.getenv('PORT'))
+HOST = os.getenv('HOST')
+PORT = int(os.getenv('PORT'))
 
 def is_white(img):
     return np.mean(img) > 200
 
 def main():
 
+    flash = False
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
+    s.connect((HOST, PORT))
 
     while True:
         video = ImageGrab.grab()
 
         if is_white(video):
-            s.send('FLASH'.encode('utf-8'))
-            print("FLASH")
-
-        cv2.imshow('window', np.array(video))
-
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
-
+            if not flash:
+                flash = True
+                s.send('FLASH'.encode('utf-8'))
+        elif flash:
+            s.send('STOP'.encode('utf-8'))
+            flash = False
 main()
